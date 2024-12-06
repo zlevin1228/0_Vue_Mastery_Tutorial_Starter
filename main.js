5893ace7d146b6ac1510ca98b09956330147ac6e
zlevin1228 <zacharylevin1228@gmail.com>
2024-12-06 13:00:58 -0500

    3. Attribute Binding COMPLETE

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            proddesc: '1 pair of 2 socks to put on your feet.'
+            image: './assets/images/socks_green.jpg'
         }
     }
 })
