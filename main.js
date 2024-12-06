4593b27fb6f25260a7b6c848cdf420b15e04d7f8
zlevin1228 <zacharylevin1228@gmail.com>
2024-12-06 12:52:55 -0500

    2. Creating The Vue App COMPLETE

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            proddesc: '1 pair of 2 socks to put on your feet.'
+        }
+    }
+})
