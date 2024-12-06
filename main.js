ad2dd1576288cfa95c1deebac6e537066a9369a9
zlevin1228 <zacharylevin1228@gmail.com>
2024-12-06 13:14:19 -0500

    5. List Rendering COMPLETE

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inventory: 0
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ]
         }
     }
 })
